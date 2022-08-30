3619cd0dd2d0baf35c36c201bdc0ff0a3c94f4ee
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-08-30 16:32:04 +0000

    convert l from mm to inches

--- ProgrammingHowToGetStarted/main.cpp
+++ ProgrammingHowToGetStarted/main.cpp
@@ -9,6 +9,7 @@ int main()
   //Initialize variables, we want to be specific     to decimal, using type double
   double P, l, S, r;
   double lin;
+  
   //Input P, l, S for Rider Force [lbs], Crank       arm   length [mm], Stress [psi]
   cout << "Enter Rider Force (P) in lbs: ";
   cin >> P;
@@ -23,7 +24,8 @@ int main()
   cout << endl;
 
   //Convert l units to inches
-
+  lin = 1/25.4;
+  
   //Calculate r = cbrt[(1*p)/(pi*S)] and pi =        3.1415926535
 
   //Display r
