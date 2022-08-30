0236f43d162fb8e2a9d4944e09434de18c1dbdbd
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-08-30 16:30:22 +0000

    Prompt the user for input

--- .ccls-cache/@home@runner@CSCI130ProgrammingHowToGetStarted/ProgrammingHowToGetStarted@main.cpp
+++ .ccls-cache/@home@runner@CSCI130ProgrammingHowToGetStarted/ProgrammingHowToGetStarted@main.cpp
@@ -2,7 +2,7 @@
 //Description: CSCI 130 – An Example of How to Solve & Approach Coding Problems
 //Date Created: 08/30/2022
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 int main() 
 {
@@ -10,7 +10,17 @@ int main()
   double P, l, S, r;
   double lin;
   //Input P, l, S for Rider Force [lbs], Crank       arm   length [mm], Stress [psi]
-  
+  cout << "Enter Rider Force (P) in lbs: ";
+  cin >> P;
+  cout << endl;
+
+  cout << "Enter Crank arm Length (l) in mm: ";
+  cin >> l;
+  cout << endl;
+
+  cout << "Enter Stress (S) in psi: ";
+  cin >> S;
+  cout << endl;
 
   //Convert l units to inches
 
