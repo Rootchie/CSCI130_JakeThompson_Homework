749de1a95822aaa4317574f24dbc0c45b860521e
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-08-30 16:14:49 +0000

    Displayed all steps in comments

---
+++ ProgrammingHowToGetStarted/main.cpp
@@ -0,0 +1,20 @@
+//Programmer: Jake Thompson
+//Description: CSCI 130 – An Example of How to Solve & Approach Coding Problems
+//Date Created: 08/30/2022
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+  //Initialize variables
+
+  //Input P, l, S for Rider Force [lbs], Crank arm length [mm], Stress [psi]
+
+  //Convert l units to inches
+
+  //Calculate r = cbrt[(1*p)/(pi*S)] and pi = 3.1415926535
+
+  //Display r
+  
+ 
+}
