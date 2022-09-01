db0241d413829080623b382e0c8b44c9b02bd4ca
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-01 16:49:42 +0000

    Included white space, indented cout(s), formatted words correctly.

--- HW2_P1/main1.cpp
+++ HW2_P1/main1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main()
 {
-cout << "Every C++ program";
-cout <<"\nmust have one and only one";
-cout << "main function";
-cout << "\n The escape sequence of characters";
-cout << "\nfor a newline can be placed anywhere";
-cout << "\n in the message passed to cout";
-return 0;
+  cout << "Every C++ program";
+  cout <<"\nmust have one and only one main function.";
+  cout << "\nThe escape sequence of ";
+  cout << "\ncharacters for a newline can ";
+  cout << "\nbe placed anywhere in the ";
+  cout << "\nmessage passed to cout.";
+  
+  return 0;
 }
