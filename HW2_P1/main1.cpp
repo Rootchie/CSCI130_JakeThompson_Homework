b929e2bef708b42a0eae5113b1c476a8cdc7d6a1
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-01 16:59:57 +0000

    added quotes, fixed some spaces, final product.

--- HW2_P1/main1.cpp
+++ HW2_P1/main1.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 int main()
+
+///////////////////////////////////////////////////
+//Every C++ program
+//must have one and only one main function.
+//The escape sequence of
+//characters for a newline can
+//be placed anywhere in the
+//message passed to cout. 
+///////////////////////////////////////////////////
+
 {
   cout << "Every C++ program";
-  cout <<"\nmust have one and only one main function.";
+  cout << "\nmust have one and only one main function.";
   cout << "\nThe escape sequence of ";
   cout << "\ncharacters for a newline can ";
   cout << "\nbe placed anywhere in the ";
