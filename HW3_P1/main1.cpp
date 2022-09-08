5a8fa105e89e4c8548ca23580bafcbae7d9b2ebc
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-08 15:32:08 +0000

    input values for equation(1) for finding C(t), and converted t(weeks) to t(days).

--- HW3_P1/main1.cpp
+++ HW3_P1/main1.cpp
@@ -12,17 +12,34 @@ int main()
   //initialize variables
   int C0;
   double Ct, A, m, r, t;
+  
   //input values for equation(2)
-  cout << "m = ";
-  cin >> m;
+  cout << "m = "; //degree of the polynomial equation
+  cin >> m; 
   cout << endl;
   
-  cout << "C(0) = ";
+  cout << "C(0) = "; //Initial disease prevelance
   cin >> C0;
   cout << endl;
+  
   //find A using equation(2), and display A value
   A = pow(C0, 1/m);
   cout << "A = " << A << endl;
+  
+  //input values for equation(1)
+  cout << "r = "; //growth rate parameter
+  cin >> r;
+  cout << endl;
+
+  cout << "t = "; // t measured in days
+  cin >> t;
+  cout << endl;
+
+  //convert t(weeks to t(days) since the time point is displayed in weeks
+  t = t*7;
+
+  
+  
   
   
   
