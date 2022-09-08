081971226d5a4214312d3fb44d029a641f7c3a8e
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-08 15:15:59 +0000

    Input values used to find A

--- HW3_P1/main1.cpp
+++ HW3_P1/main1.cpp
@@ -1,3 +1,7 @@
+//Programmer: Jake Thompson
+//Description: Practice with manipulators
+//Date: 09/05/2022
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -5,8 +9,21 @@ using namespace std;
 
 int main() 
 {
+  //initialize variables
   int C0;
   double Ct, A, m, r, t;
+  //input values for equation(2)
+  cout << "m = ";
+  cin >> m;
+  cout << endl;
+  
+  cout << "C(0) = ";
+  cin >> C0;
+  cout << endl;
+
+  
+  
+  
   
   
 
