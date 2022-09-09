4b34342c60f71799213eaa3b74cdb1558aad0bd9
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-09 18:41:14 +0000

    HW3_P2 a) initializing variables, and making user input values in console

---
+++ .ccls-cache/@home@runner@HW3Jake-Thompson/HW3_P2@main2.cpp
@@ -0,0 +1,24 @@
+//Programmer: Jake Thompson
+//Description: HW3_P1 Exercise 1 in Bronson Chapter 3
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  //
+  
+
+
+
+
+
+
+
+
+
+  
+}
---
+++ HW3_P2/main2.cpp
@@ -0,0 +1,44 @@
+//Programmer: Jake Thompson
+//Description: HW3_P1 Exercise 1 in Bronson Chapter 3
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  //initialize variables (x1,y1), (x2,y2) and S(slope)
+  double x1, y1, x2, y2, S;
+
+  //input values for the slope equation
+  cout << "x1 = ";
+  cin >> x1;
+  cout << endl;
+
+  cout << "y1 = ";
+  cin >> y1;
+  cout << endl;
+
+  cout << "x2 = ";
+  cin >> x2;
+  cout << endl;
+
+  cout << "y2 = ";
+  cin >> y2;
+  cout << endl;
+
+ 
+  
+
+
+
+
+
+
+
+
+
+  
+}
