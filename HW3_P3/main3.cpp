7347f390487473349f9d764f71c7397bfd9b4fb8
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-09 20:05:57 +0000

    HW3_P3 a) Initializing appropriate variables

--- HW3_P3/main3.cpp
+++ HW3_P3/main3.cpp
@@ -8,3 +8,18 @@
 using namespace std;
 
 int main()
+{
+  ///////////////////////////////////////////////////////
+  //Initialize variables
+  //E is the voltage of the battery in volts
+  //R is the value of the resistor in ohms
+  //C is the value of the capacitor in farads
+  //t is the time in seconds after the switch is closed
+  //e is Euler’s number, which is 2.71828 
+  //i current flowing through RC circuit
+  ///////////////////////////////////////////////////////
+  double E, R, C, t, e, i;
+
+  
+  
+}
