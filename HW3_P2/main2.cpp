b25679b1028bb0946306730a3cdb5e6e1df377f3
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-09 19:22:07 +0000

    HW3_P2 edited programmer info on top, finished product

--- HW3_P2/main2.cpp
+++ HW3_P2/main2.cpp
@@ -1,5 +1,5 @@
 //Programmer: Jake Thompson
-//Description: HW3_P1 Exercise 1 in Bronson Chapter 3
+//Description: HW3_P2 Exercise 1 in Bronson Chapter 3
 //Date: 09/09/2022
 
 #include <iostream>
---
+++ HW3_P3/main3.cpp
@@ -0,0 +1,10 @@
+//Programmer: Jake Thompson
+//Description: HW3_P3 Exercise 9 in Bronson Chapter 3
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
