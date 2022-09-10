350eae093d0087906ba046a916788c1683d1d6a7
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-10 04:25:17 +0000

    HW3_P5 Kattis sort two numbers. Reading in two integers a and b.

---
+++ HW3_P5/main5.cpp
@@ -0,0 +1,15 @@
+//Programmer: Jake Thompson
+//Description: Kattis Sort Two numbers
+//https://open.kattis.com/problems/sorttwonumbers
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  
+}
