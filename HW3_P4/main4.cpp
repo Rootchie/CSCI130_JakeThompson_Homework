0f040f6ea5874df1069e91f3eb29cefb63f4463a
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-10 03:55:46 +0000

    HW3_P4 Kattis Pizza crust. Initialized variables, and input equation for cheese percentage.

---
+++ HW3_P4/main4.cpp
@@ -0,0 +1,18 @@
+//Programmer: Jake Thompson
+//Description: Kattis Pizza Crust: https://open.kattis.com/problems/pizza2
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int R, C, p;
+  p = (((R-C)*(R-C))/(R*R))*100;
+  
+
+
+  
+}
