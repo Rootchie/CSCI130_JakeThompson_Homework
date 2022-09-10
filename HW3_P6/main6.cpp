00f9862b9b9648e23c91d5282499a90045d80b5e
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-10 06:09:52 +0000

    HW3_P6 initialized variables H and M for Hour and Minute

---
+++ HW3_P6/main6.cpp
@@ -0,0 +1,17 @@
+//Programmer: Jake Thompson
+//Description: Kattis Spavanac
+//https://open.kattis.com/problems/spavanac
+//Date: 09/09/2022
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int H, M;
+  cin >> H, M;
+  cout << endl;
+  
+}
