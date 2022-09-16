c53b62484e80e92caa87ec76474cfe4555bb2cb7
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-16 15:24:04 +0000

    reading in test cases

--- HW4_P3/main4_3.cpp
+++ HW4_P3/main4_3.cpp
@@ -11,9 +11,11 @@ using namespace std;
 int main()
 {
   //read in the # of test cases
+  int num_cases;
+  cin > num_cases;
   
   //need a loop to run all test cases
     //read in value for calculation
     //make calculation (this may require a loop)
-    //output the last digit (value % 10)
+    //find and output the last digit (value % 10)
 }
