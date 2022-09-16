ba4b03b89f16b303bd5b5b431e7490c0dea473f4
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-16 15:27:37 +0000

    declared loop variables, and read in value for calculation

--- HW4_P3/main4_3.cpp
+++ HW4_P3/main4_3.cpp
@@ -14,12 +14,19 @@ int main()
   int num_cases;
   cin >> num_cases;
   
+  //declare variables for loop
+  int fac_val;
+  
   //need a loop to run all test cases
   for (int i = 0; i < num_cases; i++)
     {
-      
-    }
     //read in value for calculation
+      cin >> fac_val;
     //make calculation (this may require a loop)
     //find and output the last digit (value % 10)
+    }
+   
+    
+    
+    
 }
