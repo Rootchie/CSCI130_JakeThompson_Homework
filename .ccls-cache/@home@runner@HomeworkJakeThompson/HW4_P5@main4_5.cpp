0cf383b293c07f097e3868c719aa0b658f3fad40
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-19 15:18:22 +0000

    made the calculation for radians more simple, by using variable (pi) instead of a calculation with cosine

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW4_P5@main4_5.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW4_P5@main4_5.cpp
@@ -1,5 +1,5 @@
 // Programmer: Jake Thompson
-// Description: HW4_P5 Kattis Jumbo Javelin
+// Description: HW4_P5 The Amazing Human Cannonball
 // https://open.kattis.com/problems/humancannonball2
 // Date: 09/18/2022
 
@@ -13,16 +13,23 @@ int main()
   // declare and read in integer cases, being number of cases
   int cases;
   cin >> cases;
+  
   // declare variables for x(t)
   double V0, Angle, deltaX, h1, h2;
+  
   // declare the variable needed to change angle from degrees to radian
-  double toRadians = acos(-1) / 180;
+  double pi = 3.14159265;
+  double toRadians = pi / 180;
+  
   // create a while that, while (cases - 1) is greater than zero, time and deltaY are calculated
   while (cases-- > 0) 
     {
     cin >> V0 >> Angle >> deltaX >> h1 >> h2;
+      
     double time = deltaX / V0 / cos(Angle * toRadians);
+      
     double deltaY = V0 * time * sin(Angle * toRadians) - .5 * 9.81 * time * time;
+      
   // if deltaY >= h1 + 1 and deltaY <= h2 - 1 output Safe
     if ((deltaY >= h1 + 1) && (deltaY <= h2 - 1))
       {
