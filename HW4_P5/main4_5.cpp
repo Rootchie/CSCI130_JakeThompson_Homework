79d9c69dd2d312e73ac76696bed04aa6c2f01fda
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-19 06:07:32 +0000

    HW4_P5 steps

---
+++ HW4_P5/main4_5.cpp
@@ -0,0 +1,24 @@
+// Programmer: Jake Thompson
+// Description: HW4_P4 Kattis Jumbo Javelin
+// https://open.kattis.com/problems/jumbojavelin
+// Date: 09/16/2022
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  // declare integer cases, being number of cases
+  
+  // declare variables for x(t)
+  
+  // declare the variable needed to change angle from degrees to radian
+  
+  // create a while that, while (cases - 1) is greater than zero, time and deltaY are calculated
+  
+  // if deltaY >= h1 + 1 and deltaY <= h2 - 1 output Safe
+  
+  // else output Not Safe
+  }
