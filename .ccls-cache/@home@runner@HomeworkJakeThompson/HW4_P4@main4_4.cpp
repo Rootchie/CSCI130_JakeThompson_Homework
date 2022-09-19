0c556ff7027e06e54aadb1a370e13f56755290b6
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-19 02:58:16 +0000

    neglected to read in N, and mistook that I had to read in l in the loop, instead of l_total. This makes more sense now, since I'm reading in l, and adding it to the total length. Then when the loop comes back over it keeps adding it to the total.

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW4_P4@main4_4.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW4_P4@main4_4.cpp
@@ -10,8 +10,9 @@ using namespace std;
 
 int main()
 {
-  //declare integer N, the number of steel rods she has.
+  //declare and read in integer N, the number of steel rods she has
   int N;
+  cin >> N;
   //declare an integer l, the length of one rod
   int l;
   //declare l_total, total length
@@ -20,10 +21,10 @@ int main()
   //loop to run test cases
   for (int i = 0; i < N; i++)
     {
-      cin >> l_total;
+      cin >> l;
       l_total += l;
     }
   //calculate total length including the 1cm part
-  cout << l_total - N - 1 << endl;
+  cout << l_total - (N - 1) << endl;
   return 0;
 }
