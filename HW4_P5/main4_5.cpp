4f017853de5a5306affa38bea92c70bce65efce6
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-19 06:17:53 +0000

    while statement (admittedly after many other while tests)

--- HW4_P5/main4_5.cpp
+++ HW4_P5/main4_5.cpp
@@ -17,12 +17,14 @@ int main()
   double V0, Angle, deltaX, h1, h2;
   // declare the variable needed to change angle from degrees to radian
   double toRadians = acos(-1) / 180;
-  
-  
   // create a while that, while (cases - 1) is greater than zero, time and deltaY are calculated
-  
+  while (cases-- > 0) 
+    {
+    cin >> V0 >> Angle >> deltaX >> h1 >> h2;
+    double time = deltaX / V0 / cos(Angle * toRadians);
+    double deltaY = V0 * time * sin(Angle * toRadians) - .5 * 9.81 * time * time;
+    }
   // if deltaY >= h1 + 1 and deltaY <= h2 - 1 output Safe
   
   // else output Not Safe
   }
-  
