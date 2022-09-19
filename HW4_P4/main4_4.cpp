848d4457d590bfa16961f4adda584dfe282c79f6
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-19 02:38:06 +0000

    HW4_P4, steps for completion

--- HW4_P4/main4_4.cpp
+++ HW4_P4/main4_4.cpp
@@ -1,7 +1,7 @@
 //Programmer: Jake Thompson
-//Description: HW4_P3
-//https://open.kattis.com/problems/lastfactorialdigit
-//Date: 09/13/2022
+//Description: HW4_P4 Kattis Jumbo Javelin
+//https://open.kattis.com/problems/jumbojavelin
+//Date: 09/16/2022
 
 #include <iostream>
 #include <cmath>
@@ -10,5 +10,14 @@ using namespace std;
 
 int main()
 {
+  //read in integer N, the number of steel rods she has.
+
+  //declare an integer l, the length of one rod
+
+  //declare l_total, total length
+
+  //loop to run test cases
+
+  //calculate total length including the 1cm part
   
 }
