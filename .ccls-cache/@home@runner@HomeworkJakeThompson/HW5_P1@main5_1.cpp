83360269b00b9b6c65c815197eb5fdca050fd477
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-21 15:51:10 +0000

    HW5_P1 stopped at step 15

---
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P1@main5_1.cpp
@@ -0,0 +1,54 @@
+// Programmer: Jake Thompson
+// Description: HW5_P1 Importing a *.dat file using a while loop
+// Date: 09/21/2022
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  // declare variables
+  string firstName;
+  string date;
+  int TestResult;
+  int count = 0;
+  int cumulative_cases = 0;
+
+  // declare stream variables
+  ifstream inFile;  // needed to access the file we want to load
+  ofstream outFile; // needed to save the results of the analysis
+
+  // open the input file
+  inFile.open("TestResultsData.dat");
+
+  // read in the date - first line file
+  inFile >> date;
+  cout << date << endl;
+
+  // read in the 2 pieces of info on the second line
+  inFile >> firstName;
+  cout << firstName << ' ';
+  inFile >> TestResult;
+  cout << TestResult << endl;
+
+  while (inFile) {
+    // update number of cases & persons tested
+    cumulative_cases = cumulative_cases + TestResult;
+    count++; // increment the number of patients
+
+    // read in the next line
+    inFile >> firstName;
+    inFile >> TestResult;
+  }
+  cout << "Total Number Cases = " << cumulative_cases << endl;
+  cout << "Number of person tested = " << count << endl;
+  inFile.close();
+
+  // open the input file
+  inFile.open("TestResultsData.dat");
+
+  // open the output file
+  outFile.open("AnalyzedData.txt");
+}
---
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P5@main5_5.cpp
@@ -0,0 +1,9 @@
+// Programmer: Jake Thompson
+// Description: 
+// 
+// Date: 09//2022
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
