a6acae19efc09311b806d9623565a9a416ffe7cd
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-26 07:02:06 +0000

    Saved, final product

--- HW5_P3/main5_3.cpp
+++ HW5_P3/main5_3.cpp
@@ -7,29 +7,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int Temp; //initialize int variable for Measured Temp
-  int WindSpeed; //Initialize int variable for wind speed
+int main() {
+  int Temp;      // initialize int variable for Measured Temp
+  int WindSpeed; // Initialize int variable for wind speed
 
-  //const double for conversion factors
+  // const double for conversion factors
   const double K1 = 13.12;
   const double K2 = 11.37;
   const double K3 = 0.3965;
-  
-  //for loop Temp
-  for (int Temp = 2; Temp <= 10; Temp += 4)
-  {
-    //for loop Wind speed
-    for (int WindSpeed = 5; WindSpeed <= 11; WindSpeed += 2)
-    {
-      //calculate wind chill
-      double WCI = K1 + 0.06125 * Temp - K2 * pow(WindSpeed, 0.16) + K3 * Temp * pow(WindSpeed, 0.16);
-      //display measured temp, wind speed, wind chill
+
+  // for loop Temp
+  for (int Temp = 2; Temp <= 10; Temp += 4) {
+    // for loop Wind speed
+    for (int WindSpeed = 5; WindSpeed <= 11; WindSpeed += 2) {
+      // calculate wind chill
+      double WCI = K1 + (0.06125 * Temp) - (K2 * pow(WindSpeed, 0.16)) + (K3 * Temp * pow(WindSpeed, 0.16));
+      // display measured temp, wind speed, wind chill
       cout << "Measured Temperature = " << Temp << " C" << endl;
       cout << "Wind Speed = " << WindSpeed << " km/hr" << endl;
       cout << "Wind Chill Ind. = " << WCI << endl;
     }
-  }    
+  }
   return 0;
 }
