2d789fcbf8d0d491adb69c6ce2ee8ab275201f49
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-26 06:40:54 +0000

    HW5_P3 instructions

--- HW5_P3/main5_3.cpp
+++ HW5_P3/main5_3.cpp
@@ -1,9 +1,21 @@
 // Programmer: Jake Thompson
-// Description: 
-// 
-// Date: 09//2022
+// Description: HW5_P3 Bronson Exercises 5.6 problem 6
+// Date: 09/25/2022
 
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 using namespace std;
+
+int main()
+{
+  //initialize int variable for Measured Temp
+  //Initialize int variable for wind speed
+
+  //const double for conversion factors
+
+  //for loop Temp
+    //for loop Wind speed
+    //calculate wind speed
+    //display measured temp, wind speed, wind chill
+}
