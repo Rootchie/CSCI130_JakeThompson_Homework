b576e8d32957c319bd98cfe90af8ac4d7aa8eb9f
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-26 07:34:11 +0000

    final product!

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P4@main5_4.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P4@main5_4.cpp
@@ -8,26 +8,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  bool modulo [42];
+int main() {
+  bool modulo[42];
 
-  for (int i = 0; i < 42; i++) //loop for bool modulo [42] value
+  for (int i = 0; i < 42; i++) // loop for bool modulo [42] value
   {
-    modulo [i] = false;
+    modulo[i] = false;
   }
-  for (int k = 0; k < 10; k++) //use mod math with number inputs
-  {                            //then output true statement for modulo [42]
+  for (int k = 0; k < 10; k++) // use mod math with number inputs
+  {                            // then output true statement for modulo [42]
     int num_values;
-    cin >> num_values; //input ten values into for loop for mod math
-    modulo [num_values % 42] = true;
+    cin >> num_values; // input ten values into for loop for mod math
+    modulo[num_values % 42] = true;
   }
   int dist_value = 0;
 
-  for (int g = 0; g < 42; g++)
-  {
-    if (modulo [g]) //if modulo is less than 42
-                    //then distinct values add up until limit is reached
+  for (int g = 0; g < 42; g++) {
+    if (modulo[g]) // if modulo is less than 42
+    // then distinct values add up until limit is reached
     {
       dist_value++;
     }
