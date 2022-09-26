6eafac5e42b5369ec75611455769128e276b2a6a
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-26 06:16:33 +0000

    HW5_P2 #6

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P2@main5_2.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW5_P2@main5_2.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main()
 {
   int dataTemp;
+  int sumData = 0;
+  int counter = 0;
   
   // load input file
   ifstream inFile; // declare stream variable
@@ -18,7 +20,17 @@ int main()
   // load all data in while loop
   while (inFile >> dataTemp) 
   {
-    inFile >> dataTemp;
-    //cout << dataTemp << endl;
+    // check to see if data is valid
+    if (dataTemp >= 0)
+    {
+      //update cumulative value of all data and number of points loaded
+      
+    }
+    else //the value is negative and we should exit
+    {
+      cout << "Negative value encountered of " << dataTemp << "." << endl;
+      cout << "Will not use the rest of the data... " << endl;
+      break;
+    }
   }
 }
