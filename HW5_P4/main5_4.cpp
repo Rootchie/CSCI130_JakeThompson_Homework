52866f6a9a3e04a9d099a5d62b6686b2236d620c
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-26 07:18:10 +0000

    HW5_P4 bool modulo [42], when the numbers are considered modulo 42. created for loop for bool modulo [42]

--- HW5_P4/main5_4.cpp
+++ HW5_P4/main5_4.cpp
@@ -1,9 +1,20 @@
 // Programmer: Jake Thompson
-// Description: 
-// 
-// Date: 09//2022
+// Description: HW5_P4 Kattis Modulo
+// https://open.kattis.com/problems/modulo
+// Date: 09/25/2022
 
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 using namespace std;
+
+int main()
+{
+  bool modulo [42];
+  cout << "Enter ten number values: " << endl;
+
+  for (int i = 0; i < 42; i++) //loop for bool modulo [42] value
+  {
+    modulo [1] = false;
+  }
+}
