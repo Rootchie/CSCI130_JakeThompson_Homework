e5bb1a853d164ae8dea871b2847d455040b82319
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-29 23:50:11 +0000

    HW6_P3 added units (changed from J/s to Watts), final product

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P3@main6_3.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P3@main6_3.cpp
@@ -12,9 +12,9 @@ double powerGen(double H, double Q);
 int main()
 {
   //test 1
-  cout << powerGen(14, 20) << endl;
+  cout << powerGen(14, 20) << " Watts" << endl;
   //test 2
-  cout << powerGen(30, 20) << endl;
+  cout << powerGen(30, 20) << " Watts" << endl;
   return 0;
 }
 
@@ -25,5 +25,5 @@ double powerGen(double H, double Q)
   double WaterDensity = 1000;
   return WaterDensity * Q * H * g;
 }
-//test 1 = 2.7468e+06, or 2746800 J/s
-//test 2 = 2.886e+06, or 5886000 J/s
+//test 1 = 2.7468e+06, or 2746800 Watts
+//test 2 = 2.886e+06, or 5886000 Watts
