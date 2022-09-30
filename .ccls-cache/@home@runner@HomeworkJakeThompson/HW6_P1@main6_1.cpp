eeee8a023c4bbf19714fbedf3cd11a1bd42f94aa
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-09-30 21:01:22 +0000

    Put the random message display code in a function DetermineWinner, testing

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P1@main6_1.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P1@main6_1.cpp
@@ -15,6 +15,7 @@ using namespace std;
 void TranslatePick(int flag_who, char selection);
 char ComputerSelectRPSLK();
 void DetermineWinner(char userPick, char computerPick);
+void GiveFeedback(char userPick, char computerPick);
 //////////////////// MAIN ////////////////////////////////////////////////////
 int main() {
   // declare vars
@@ -51,8 +52,135 @@ int main() {
   // HANDOUT (PART B): Inform the user who won.
   DetermineWinner(userPick, computerPick);
 
-  // HANDOUT (PART C): Randomly display celebratory messages if user won.
-  int MessagePick;
+  // HANDOUT (PART C): Randomly display celebratory messages if user won, lost or tied.
+GiveFeedback(userPick, computerPick);
+  return 0;
+}
+
+/////////////// User-Created Functions ////////////////////////////////////////
+
+/* Descprition: This function is used to translate the character selection to a
+        statement which is displayed to the user.
+   Parameters:  flag_who is an integer which is used to indicate if the computer
+                         or the user made the choice.
+                selection is one of the 5 valid character selections  */
+void TranslatePick(int flag_who, char selection) {
+  // declare local function variables
+  string who_text;
+
+  // identify which statements to insert into the output below
+  if (flag_who == 0)
+    who_text = "You";
+  else
+    who_text = "The Computer";
+
+  // display output where the char selection is translated into words
+  if (selection == 'R')
+    cout << who_text << " selected Rock." << endl;
+  else if (selection == 'P')
+    cout << who_text << " selected Paper." << endl;
+  else if (selection == 'S')
+    cout << who_text << " selected Scissors." << endl;
+  else if (selection == 'L')
+    cout << who_text << " selected Lizard." << endl;
+  else if (selection == 'K')
+    cout << who_text << " selected Spock." << endl;
+  else // something is wrong
+    cout << "Check code for errors. Message from TranslatePick fcn." << endl;
+
+  return;
+}
+
+/* Descprition: This function is used to make a random character selection in
+   the game Rock, Paper, Scissors, Lizard, Spock Parameters:  there are no
+   inputs */
+char ComputerSelectRPSLK() {
+  // declare local function variables
+  char randPickChar;
+  int randPick;
+
+  // pick a random number from 0 to 4
+  srand(time(NULL));     // initialize random seed.
+  randPick = rand() % 5; // gives us 0 to 4
+  // cout << randPick;   // uncomment to debug
+
+  // assign random number to letter
+  switch (randPick) {
+  case 0:
+    randPickChar = 'R';
+    break;
+  case 1:
+    randPickChar = 'P';
+    break;
+  case 2:
+    randPickChar = 'S';
+    break;
+  case 3:
+    randPickChar = 'L';
+    break;
+  case 4:
+    randPickChar = 'K';
+    break;
+  default:
+    cout << "Error in the function ComputerSelectRPSLK. " << endl;
+  }
+
+  return randPickChar;
+}
+ void DetermineWinner(char userPick, char computerPick)
+{
+if (userPick == 'R') // When user picks Rock
+  {
+    if (computerPick == 'R')
+      cout << "Tie." << endl;
+    else if ((computerPick == 'K') || (computerPick == 'P'))
+      cout << "You Lose." << endl;
+    else
+      cout << "You Win." << endl;
+  }
+
+  if (userPick == 'P') // When user picks Paper
+  {
+    if (computerPick == 'P')
+      cout << "Tie." << endl;
+    else if ((computerPick == 'S') || (computerPick == 'L'))
+      cout << "You Lose." << endl;
+    else
+      cout << "You Win." << endl;
+  }
+
+  if (userPick == 'S') // When user picks Scissors
+  {
+    if (computerPick == 'S')
+      cout << "Tie." << endl;
+    else if ((computerPick == 'R') || (computerPick == 'K'))
+      cout << "You Lose." << endl;
+    else
+      cout << "You Win." << endl;
+  }
+
+  if (userPick == 'L') // When user picks Lizard
+  {
+    if (computerPick == 'L')
+      cout << "Tie." << endl;
+    else if ((computerPick == 'R') || (computerPick == 'S'))
+      cout << "You Lose." << endl;
+    else
+      cout << "You Win." << endl;
+  }
+
+  if (userPick == 'K') {
+    if (computerPick == 'K')
+      cout << "Tie." << endl;
+    else if ((computerPick == 'P') || (computerPick == 'L'))
+      cout << "You Lose." << endl;
+    else
+      cout << "You Win." << endl;
+  }
+}
+void GiveFeedback(char userPick, char computerPick)
+{
+int MessagePick;
   string RandMessage;
   srand(time(NULL));
   if (userPick == 'R') // When user picks Rock
@@ -298,127 +426,4 @@ int main() {
     }
       
   }  
-  return 0;
-}
-
-/////////////// User-Created Functions ////////////////////////////////////////
-
-/* Descprition: This function is used to translate the character selection to a
-        statement which is displayed to the user.
-   Parameters:  flag_who is an integer which is used to indicate if the computer
-                         or the user made the choice.
-                selection is one of the 5 valid character selections  */
-void TranslatePick(int flag_who, char selection) {
-  // declare local function variables
-  string who_text;
-
-  // identify which statements to insert into the output below
-  if (flag_who == 0)
-    who_text = "You";
-  else
-    who_text = "The Computer";
-
-  // display output where the char selection is translated into words
-  if (selection == 'R')
-    cout << who_text << " selected Rock." << endl;
-  else if (selection == 'P')
-    cout << who_text << " selected Paper." << endl;
-  else if (selection == 'S')
-    cout << who_text << " selected Scissors." << endl;
-  else if (selection == 'L')
-    cout << who_text << " selected Lizard." << endl;
-  else if (selection == 'K')
-    cout << who_text << " selected Spock." << endl;
-  else // something is wrong
-    cout << "Check code for errors. Message from TranslatePick fcn." << endl;
-
-  return;
-}
-
-/* Descprition: This function is used to make a random character selection in
-   the game Rock, Paper, Scissors, Lizard, Spock Parameters:  there are no
-   inputs */
-char ComputerSelectRPSLK() {
-  // declare local function variables
-  char randPickChar;
-  int randPick;
-
-  // pick a random number from 0 to 4
-  srand(time(NULL));     // initialize random seed.
-  randPick = rand() % 5; // gives us 0 to 4
-  // cout << randPick;   // uncomment to debug
-
-  // assign random number to letter
-  switch (randPick) {
-  case 0:
-    randPickChar = 'R';
-    break;
-  case 1:
-    randPickChar = 'P';
-    break;
-  case 2:
-    randPickChar = 'S';
-    break;
-  case 3:
-    randPickChar = 'L';
-    break;
-  case 4:
-    randPickChar = 'K';
-    break;
-  default:
-    cout << "Error in the function ComputerSelectRPSLK. " << endl;
-  }
-
-  return randPickChar;
-}
- void DetermineWinner(char userPick, char computerPick)
-{
-if (userPick == 'R') // When user picks Rock
-  {
-    if (computerPick == 'R')
-      cout << "Tie." << endl;
-    else if ((computerPick == 'K') || (computerPick == 'P'))
-      cout << "You Lose." << endl;
-    else
-      cout << "You Win." << endl;
-  }
-
-  if (userPick == 'P') // When user picks Paper
-  {
-    if (computerPick == 'P')
-      cout << "Tie." << endl;
-    else if ((computerPick == 'S') || (computerPick == 'L'))
-      cout << "You Lose." << endl;
-    else
-      cout << "You Win." << endl;
-  }
-
-  if (userPick == 'S') // When user picks Scissors
-  {
-    if (computerPick == 'S')
-      cout << "Tie." << endl;
-    else if ((computerPick == 'R') || (computerPick == 'K'))
-      cout << "You Lose." << endl;
-    else
-      cout << "You Win." << endl;
-  }
-
-  if (userPick == 'L') // When user picks Lizard
-  {
-    if (computerPick == 'L')
-      cout << "Tie." << endl;
-    else if ((computerPick == 'R') || (computerPick == 'S'))
-      cout << "You Lose." << endl;
-    else
-      cout << "You Win." << endl;
-  }
-
-  if (userPick == 'K') {
-    if (computerPick == 'K')
-      cout << "Tie." << endl;
-    else if ((computerPick == 'P') || (computerPick == 'L'))
-      cout << "You Lose." << endl;
-    else
-      cout << "You Win." << endl;
-  }
 }
