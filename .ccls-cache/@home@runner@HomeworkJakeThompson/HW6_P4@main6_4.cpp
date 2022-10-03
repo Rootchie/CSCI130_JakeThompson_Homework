16516c80b1280313ce7fa03b8311b7d97a972610
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-03 04:58:45 +0000

    Function to run each input through each astrological interval

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P4@main6_4.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW6_P4@main6_4.cpp
@@ -10,4 +10,13 @@ using namespace std;
 
 int main()
 {
+  
 }
+void zodiac_sign(int day, string month)
+{
+    string astro_sign="";
+     
+    // checks month and date within the
+    // valid range of a specified zodiac
+    cin >> day;
+    cin >> month;
