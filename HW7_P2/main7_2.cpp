110ca48981d330c8e9be1b493b5c8e512e1dbdf3
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-11 19:16:22 +0000

    HW7_P2 Saved the code and the only noticeable thing that changed was the & moved one space to the left. FINAL PRODUCT

--- HW7_P2/main7_2.cpp
+++ HW7_P2/main7_2.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-void time(int seconds, int& hours, int& mins, int& secs);
+void time(int seconds, int &hours, int &mins, int &secs);
 int main() {
   int hours, seconds, mins, secs;
   cout << "Input seconds: ";
@@ -14,7 +14,7 @@ int main() {
   time(seconds, hours, mins, seconds);
   return 0;
 }
-void time(int seconds, int& hours, int& mins, int& secs) {
+void time(int seconds, int &hours, int &mins, int &secs) {
   // how many hours in seconds
   hours = seconds / 3600;
   // subtract hours from seconds
@@ -24,10 +24,10 @@ void time(int seconds, int& hours, int& mins, int& secs) {
   seconds -= mins * 60;
   // remainder of seconds = secs
   secs = seconds;
-  
+
   cout << "Equivalent number of hours = " << hours << endl;
   cout << "Equivalent number of minutes = " << mins << endl;
   cout << "Remaining seconds = " << secs << endl;
-  
+
   return;
 }
