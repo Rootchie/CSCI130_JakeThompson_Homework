0b27eb8c554088e2aeb0b2dd783c67c5d4bae480
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-11 18:19:47 +0000

    HW7_P2 created function time( ) with parameters.

---
+++ HW7_P2/main7_2.cpp
@@ -0,0 +1,12 @@
+// Programmer: Jake Thompson
+// Description: Bronson Exercises 6.3 Exercise (Practice)
+// Date: 10/11/2022
+
+#include <iostream>
+using namespace std;
+
+void time(int seconds, int hours, int mins, int secs);
+int main() {
+  
+}
+ void time(int seconds, int hours, int mins, int secs)
