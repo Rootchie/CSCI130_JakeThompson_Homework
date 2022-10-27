65871ffef9dd8497866e223b2fd79dbf598fadf9
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-27 05:00:49 +0000

    Made a function printMatrix to display the table in the console. Did multiple variations of this that didn't work, and neglected to commit, so this is the final version of printMatrix

--- HW9_P1/main9_1.cpp
+++ HW9_P1/main9_1.cpp
@@ -5,12 +5,25 @@
 #include <iomanip>
 using namespace std;
 
+void printMatrix(int matrix[][5], int N_ROWS, int N_COLUMNS);
+
 int main() {
   const int N_ROWS = 5;
   const int N_COLUMNS = 5;
-  int grade[N_ROWS][N_COLUMNS] = {{1, 100, 100, 100, 100},
-                                  {2, 100, 0, 100, 0},
-                                  {3, 82, 94, 73, 86},
-                                  {4, 64, 74, 84, 94},
-                                  {5, 94, 84, 74, 64}};
+  int grade[N_ROWS][N_COLUMNS] =  {{1, 100, 100, 100, 100},
+                                   {2, 100, 0, 100, 0},
+                                   {3, 82, 94, 73, 86},
+                                   {4, 64, 74, 84, 94},
+                                   {5, 94, 84, 74, 64}};
+
+  printMatrix(grade, N_ROWS, N_COLUMNS);
+}
+
+void printMatrix(int matrix[][5], int N_ROWS, int N_COLUMNS) {
+  int row, col;
+  for (row = 0; row < N_ROWS; row++) {
+    for (col = 0; col < N_COLUMNS; col++) 
+      cout << setw(6) << matrix[row][col] << " ";
+      cout << endl;
+  }
 }
