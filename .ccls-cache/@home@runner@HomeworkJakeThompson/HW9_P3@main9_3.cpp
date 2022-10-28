f1061dd8591dc85ee12ee6203d4a24ea0f4ffdb2
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-28 01:48:50 +0000

    HW9_P3 initialize array parameters, and answers in array

---
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW9_P3@main9_3.cpp
@@ -0,0 +1,14 @@
+// Programmer: Jake Thompson
+// Description:  Bronson Exercises Project 12
+// Date: 10/27/2022\
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int numQuestions = 5;
+char answers[numQuestions] = {'T','T','F','F','T'};
+
+int main() {
+  
+}
