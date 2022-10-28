7197a1c76f3269c8e3fa34ce6b3043e7dfc14a09
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-10-28 05:13:59 +0000

    If the determinant is zero, cout an error statement

--- HW9_P5/main9_5.cpp
+++ HW9_P5/main9_5.cpp
@@ -25,5 +25,19 @@ int main()
         cout << "Determinant cannot be ZERO" << endl;
         return 0;
     }
+    /* Find adjoint of matrix A */
+    adjA[0][0]=A[1][1];
+    adjA[1][1]=A[0][0];
+    adjA[0][1]=-A[0][1];
+    adjA[1][0]=-A[1][0];
+    cout << "Matrix A is: " << endl;
+    for (i = 0;i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            cout << setw(2) << A[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
