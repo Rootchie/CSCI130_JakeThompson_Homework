d933e22f7a060044789f47c1a553a42c45572b90
Rootchie <111894164+Rootchie@users.noreply.github.com>
2022-11-02 07:58:40 +0000

    read in array

--- .ccls-cache/@home@runner@HomeworkJakeThompson/HW10_P3@main10_3.cpp
+++ .ccls-cache/@home@runner@HomeworkJakeThompson/HW10_P3@main10_3.cpp
@@ -8,6 +8,22 @@
 
 using namespace std;
 
+int count = 0;
+
 int main() {
+  int newMax, newArraySize;
+  int N;
+  cout << "Enter size of N: ";
+  cin >> N;
+
+  const int ARRAY_SIZE = N;
+  int myArray[ARRAY_SIZE];
+  int myNewArray[] = {0};
+
+  cout << "Enter " << N << " integers: ";
+  for (int index = 0; index < ARRAY_SIZE; index++) {
+    cin >> myArray[index];
+  }
   
 }
+
